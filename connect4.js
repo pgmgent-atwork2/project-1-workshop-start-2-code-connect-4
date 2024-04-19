1af6943b1d11927b1b2841e3f1d4b81283b0c5a1
PGM-KutluEnes <eneskutl@student.arteveldehs.be>
2024-04-19 09:50:53 +0200

    update js

--- connect4.js
+++ connect4.js
@@ -1,23 +1,25 @@
 const player1 = "Mario";
 const player2 = "Peach";
 const currPlayer = player1;
-
+ 
 const gameOver = false;
 let board;
-
+ 
 const rows = 6;
 const columns = 7;
 const currColums = [];
-
+ 
 window.onload = function() {
     setGame();
 };
-
+ 
 function setGame() {
     board = [];
-    columns = [5,5,5,5,5,5,5]
+    currColums = [5, 5, 5, 5, 5, 5,];
+   
     for (let r = 0; r < rows; r++) {
         let row = [];
+       
         for (let c = 0; c < columns; c++) {
             row.push(' ');
             let tile = document.createElement("div");
@@ -25,20 +27,28 @@ function setGame() {
             tile.classList.add("tile");
             tile.addEventListener("click", setPiece);
             document.getElementById("board").append(tile);
-            }
+        }
         board.push(row);
     }
 }
-
+ 
 function setPiece() {
     if (gameOver) {
         return;
-    } 
+    }
+
     let coords = this.id.split("-");
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
+ 
+   
     r = currColums[c];
-
+    }
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]); 
+    let c = parseInt(coords[1]);
+    r = currColums[c];
+ 
     if (r < 0) {
         return;
     }
@@ -54,4 +64,13 @@ function setPiece() {
     r -= 1;
     currColums[c] = r;
     checkWinner();
+
+    function setWinner(r, c) {
+        let winner = document.getElementById("winner");
+        if (board[r][c] === player1) {
+            winner.innerText = "Mario Wins";
+        } else {
+            winner.innerText = "Peach Wins";
+        }
+        gameOver = true; 
 }
